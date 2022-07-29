70ac31bbcc09767ab7ece44ae726c40b0e086c17
v.shorinadm <voldemar-96@mail.ru>
2022-07-29 14:27:12 +1000

    Учебное приложение с использованием React.js

---
+++ src/components/pages/people-page.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { PersonDetails, PersonList } from '../sw-components';
+import { withRouter } from 'react-router-dom';
+import Row from '../row';
+
+const PeoplePage = ({history, match}) => {
+        const {id} = match.params;
+        return (
+            <Row
+                left={<PersonList onItemSelected={(id)=>history.push(id)} />}
+                right={<PersonDetails itemId={id} />} />
+        );
+}
+
+export default withRouter(PeoplePage);
---
+++ src/components/starship-details/starship-details.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './starship-details.css';
+import {useLocation, useMatch, useHistory, useParams} from 'react-router-dom';
+
+export default function StarshipDetails() {
+    const params = useParams();
+    return(
+        <h1>123</h1>
+    )
+}
